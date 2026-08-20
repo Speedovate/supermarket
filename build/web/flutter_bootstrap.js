b9a7f717ad4c919c6fdf51da78eff1bd4f518a28
Adryc Allen Catapang <catapangadrycallen@gmail.com>
2026-08-21 05:30:01 +0800

    wip

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -36,11 +36,20 @@ if (!window._flutter) {
 _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48ab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 
-const splash = document.getElementById('app-startup-splash');
-let splashRemoved = false;
 const userAgent = navigator.userAgent || "";
-const isMetaInAppBrowser =
-  /FBAN|FBAV|FB_IAB|Messenger|Instagram|Line|MicroMessenger|WeChat|Twitter|Telegram|Viber|Snapchat|LinkedInApp|Pinterest|wv\)|; wv|TikTok/i.test(userAgent);
+const isSocialInAppBrowser =
+  /FBAN|FBAV|Messenger|Instagram|Line\/|Line |MicroMessenger|WeChat|Viber|Telegram|Twitter|X\/|Snapchat|LinkedInApp|Pinterest/i.test(
+    userAgent,
+  );
+
+window.__ANDREWS_SOCIAL_INAPP__ = isSocialInAppBrowser;
+
+if (isSocialInAppBrowser) {
+  document.documentElement.setAttribute("data-social-inapp", "true");
+}
+
+const splash = document.getElementById("app-startup-splash");
+let splashRemoved = false;
 
 function removeSplashWhenFirstFrameIsLikelyReady() {
   if (!splash || splashRemoved) {
@@ -49,11 +58,11 @@ function removeSplashWhenFirstFrameIsLikelyReady() {
   splashRemoved = true;
 
   const remove = () => {
-    splash.style.opacity = '0';
-    splash.style.transition = 'opacity 120ms ease-out';
+    splash.style.opacity = "0";
+    splash.style.transition = "opacity 120ms ease-out";
     window.setTimeout(() => {
       splash.remove();
-      if (typeof window.__syncThemeColor === 'function') {
+      if (typeof window.__syncThemeColor === "function") {
         window.__syncThemeColor();
       }
     }, 120);
@@ -66,25 +75,32 @@ function removeSplashWhenFirstFrameIsLikelyReady() {
   });
 }
 
-_flutter.loader.load({
-  config: {
-    canvasKitBaseUrl: "canvaskit",
-    renderer: isMetaInAppBrowser ? "canvaskit" : undefined,
-  },
-  onEntrypointLoaded: async function(engineInitializer) {
-    const fallbackRemovalTimer = window.setTimeout(
-      removeSplashWhenFirstFrameIsLikelyReady,
-      4000,
-    );
-    window.addEventListener(
-      'flutter-first-frame',
-      () => {
-        window.clearTimeout(fallbackRemovalTimer);
-        removeSplashWhenFirstFrameIsLikelyReady();
-      },
-      { once: true },
-    );
-    const appRunner = await engineInitializer.initializeEngine();
-    await appRunner.runApp();
-  }
-});
+async function loadFlutterApp() {
+  await _flutter.loader.load({
+    config: {
+      canvasKitBaseUrl: "canvaskit/",
+    },
+    onEntrypointLoaded: async function (engineInitializer) {
+      const fallbackRemovalTimer = window.setTimeout(
+        removeSplashWhenFirstFrameIsLikelyReady,
+        4000,
+      );
+      window.addEventListener(
+        "flutter-first-frame",
+        () => {
+          window.clearTimeout(fallbackRemovalTimer);
+          removeSplashWhenFirstFrameIsLikelyReady();
+        },
+        { once: true },
+      );
+      const appRunner = await engineInitializer.initializeEngine();
+      await appRunner.runApp();
+    },
+  });
+}
+
+window.__ANDREWS_START_FLUTTER__ = loadFlutterApp;
+
+if (!isSocialInAppBrowser) {
+  window.__ANDREWS_START_FLUTTER__();
+}
--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,11 +1,20 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const splash = document.getElementById('app-startup-splash');
-let splashRemoved = false;
 const userAgent = navigator.userAgent || "";
-const isMetaInAppBrowser =
-  /FBAN|FBAV|FB_IAB|Messenger|Instagram|Line|MicroMessenger|WeChat|Twitter|Telegram|Viber|Snapchat|LinkedInApp|Pinterest|wv\)|; wv|TikTok/i.test(userAgent);
+const isSocialInAppBrowser =
+  /FBAN|FBAV|Messenger|Instagram|Line\/|Line |MicroMessenger|WeChat|Viber|Telegram|Twitter|X\/|Snapchat|LinkedInApp|Pinterest/i.test(
+    userAgent,
+  );
+
+window.__ANDREWS_SOCIAL_INAPP__ = isSocialInAppBrowser;
+
+if (isSocialInAppBrowser) {
+  document.documentElement.setAttribute("data-social-inapp", "true");
+}
+
+const splash = document.getElementById("app-startup-splash");
+let splashRemoved = false;
 
 function removeSplashWhenFirstFrameIsLikelyReady() {
   if (!splash || splashRemoved) {
@@ -14,11 +23,11 @@ function removeSplashWhenFirstFrameIsLikelyReady() {
   splashRemoved = true;
 
   const remove = () => {
-    splash.style.opacity = '0';
-    splash.style.transition = 'opacity 120ms ease-out';
+    splash.style.opacity = "0";
+    splash.style.transition = "opacity 120ms ease-out";
     window.setTimeout(() => {
       splash.remove();
-      if (typeof window.__syncThemeColor === 'function') {
+      if (typeof window.__syncThemeColor === "function") {
         window.__syncThemeColor();
       }
     }, 120);
@@ -31,25 +40,32 @@ function removeSplashWhenFirstFrameIsLikelyReady() {
   });
 }
 
-_flutter.loader.load({
-  config: {
-    canvasKitBaseUrl: "canvaskit",
-    renderer: isMetaInAppBrowser ? "canvaskit" : undefined,
-  },
-  onEntrypointLoaded: async function(engineInitializer) {
-    const fallbackRemovalTimer = window.setTimeout(
-      removeSplashWhenFirstFrameIsLikelyReady,
-      4000,
-    );
-    window.addEventListener(
-      'flutter-first-frame',
-      () => {
-        window.clearTimeout(fallbackRemovalTimer);
-        removeSplashWhenFirstFrameIsLikelyReady();
-      },
-      { once: true },
-    );
-    const appRunner = await engineInitializer.initializeEngine();
-    await appRunner.runApp();
-  }
-});
+async function loadFlutterApp() {
+  await _flutter.loader.load({
+    config: {
+      canvasKitBaseUrl: "canvaskit/",
+    },
+    onEntrypointLoaded: async function (engineInitializer) {
+      const fallbackRemovalTimer = window.setTimeout(
+        removeSplashWhenFirstFrameIsLikelyReady,
+        4000,
+      );
+      window.addEventListener(
+        "flutter-first-frame",
+        () => {
+          window.clearTimeout(fallbackRemovalTimer);
+          removeSplashWhenFirstFrameIsLikelyReady();
+        },
+        { once: true },
+      );
+      const appRunner = await engineInitializer.initializeEngine();
+      await appRunner.runApp();
+    },
+  });
+}
+
+window.__ANDREWS_START_FLUTTER__ = loadFlutterApp;
+
+if (!isSocialInAppBrowser) {
+  window.__ANDREWS_START_FLUTTER__();
+}
